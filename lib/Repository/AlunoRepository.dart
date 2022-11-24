7045897df39be75ce3a2be2fb1b82fcdd16c1432
Vinicius <vinibranger@gmail.com>
2022-11-24 20:48:59 -0300

    images

--- lib/Repository/AlunoRepository.dart
+++ lib/Repository/AlunoRepository.dart
@@ -3,10 +3,10 @@ import '../shared/models/cliente_Aluno.dart';
 class AlunoRepository {
   static List<Aluno_cliente> tabela = [
     Aluno_cliente(
-        icone_aluno: 'assets/call.png', name: 'VINI', genero: 'Masculino'),
+        icone_aluno: 'assets/user.png', name: 'VINI', genero: 'Masculino'),
     Aluno_cliente(
-        icone_aluno: 'assets/envelope.png', name: 'test', genero: 'Masculino'),
+        icone_aluno: 'assets/user.png', name: 'João', genero: 'Masculino'),
     Aluno_cliente(
-        icone_aluno: 'assets/location.png', name: 'ola', genero: 'feminino'),
+        icone_aluno: 'assets/user.png', name: 'Paula', genero: 'Feminino'),
   ];
 }
